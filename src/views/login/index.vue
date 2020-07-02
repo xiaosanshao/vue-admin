603e87e41a0833bff04e18559a135052d9a11891
unknown <568382948@qq.com>
2020-07-02 15:26:04 +0800

    初始化

---
+++ src/views/login/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="login">这是登录页
+         <el-button>默认按钮</el-button>
+    </div>
+   
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+#login{
+    height: 100vh;
+    background-color: darkslategrey;
+}
+</style>
